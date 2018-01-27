b3213223623674c12ebe45d6901e626717781aa6
mystery <mystery@mysteryPC.fritz.box>
2018-01-28 00:47:27 +0100

    Added food and energy usage/production to building, planets and ships

--- game.c
+++ game.c
@@ -53,6 +53,7 @@ struct ShipClass
 	float weaponRange;
 	float fireSpeed;
 	float baseHealth;
+	float energyUsage;
 };
 
 struct Ship
@@ -407,7 +408,7 @@ void newGame(int seed, float galaxyRadius, int planets)
 	for (int i = 0; i < game.numPlanets; i++)
 	{
 		Planet* planet = &game.planets[i];
-		planet->team = i < 1 ? 0 : 1;
+		planet->team = 1;
 		srand(game.planets[i].seed);
 		// positioning
 		planet->position = vecscale(vecf(cos(a), sin(a)), sqrt(r / game.galaxyRadius) * game.galaxyRadius);
@@ -425,6 +426,12 @@ void newGame(int seed, float galaxyRadius, int planets)
 			tile->buildingLevel = 0;
 		}
 	}
+
+	// set up starting planet
+	Planet* p = &game.planets[0];
+	p->team = 0;
+	p->tiles[p->numTiles/2].buildingType = 1;
+	p->tiles[p->numTiles/2].buildingLevel = 1;
 
 	game.nextWave.spawnAreaP1 = vecf(-galaxyRadius/10.f, galaxyRadius);
 	game.nextWave.spawnAreaP2 = vecf( galaxyRadius/10.f, galaxyRadius);
@@ -496,56 +503,60 @@ void tickGame(float step)
 	Vectorf resource_delta = vecf(0, 0, 0);
 
 	// tick planets
+	bool tick = false; // this is only true during one cycle per second and used to determine when to spawn ships
 	if ((int)game.gameAge != (int)(game.gameAge+step))
 	{
+		tick = true;
 		printf("tick! %d %d %f\n", (int)game.gameAge, (int)(game.gameAge+step), game.gameAge+step);
-		for (int i = 0; i < game.numPlanets; i++)
+	}
+	for (int i = 0; i < game.numPlanets; i++)
+	{
+		for (int j = 0; j < game.planets[i].numTiles; j++)
 		{
-			for (int j = 0; j < game.planets[i].numTiles; j++)
+			switch (game.planets[i].tiles[j].buildingType)
 			{
-				switch (game.planets[i].tiles[j].buildingType)
-				{
-					case 0: // ignore, empty tile
-						break;
-					case 1: // HQ, todo: add special effects
-						game.resources[rsc_energy] += 1 * step;
-						game.resources[rsc_sbm] += 1 * step;
-						game.resources[rsc_food] += 1 * step;
-						break;
-					case 2: // mine, todo: add sbm production
-						game.resources[rsc_sbm] += 3 * step;
-						break;
-					case 3: // power plant, todo: add energy production
-						game.resources[rsc_energy] += 3 * step;
-						break;
-					case 4: // farm, todo: add food production
-						game.resources[rsc_food] += 3 * step;
-						break;
-					case 5: // shipyard(fighter), produces 1 ship every 5 seconds
-						if (((int)game.gameAge) % 2 == 0 && game.planets[i].shipPresence[0] < game.planets[i].radius)
-						{
-							float a = (rand() % 360) / (180.f/3.41f);
-							Ship* s = spawnShip(vecadd(vecf(cos(a)*game.planets[i].radius, sin(a)* game.planets[i].radius), game.planets[i].position), 0, 0);
-							s->velocity = normalize(vecsub(s->position, game.planets[i].position));
-						}
-						break;
-					case 6: // shipyard(bomber), produces 1 ship every 15 seconds
-						if (((int)game.gameAge) % 10 == 0 && game.planets[i].shipPresence[1] < game.planets[i].radius)
-						{
-							float a = (rand() % 360) / (180.f/3.41f);
-							Ship* s = spawnShip(vecadd(vecf(cos(a)*game.planets[i].radius, sin(a)* game.planets[i].radius), game.planets[i].position), 1, 0);
-							s->velocity = normalize(vecsub(s->position, game.planets[i].position));
-						}
-						break;
-					case 7: // shipyard(cruiser), produces 1 ship every 60 seconds
-						if (((int)game.gameAge) % 30 == 0 && game.planets[i].shipPresence[2] < game.planets[i].radius)
-						{
-							float a = (rand() % 360) / (180.f/3.41f);
-							Ship* s = spawnShip(vecadd(vecf(cos(a)*game.planets[i].radius, sin(a)* game.planets[i].radius), game.planets[i].position), 2, 0);
-							s->velocity = normalize(vecsub(s->position, game.planets[i].position));
-						}
-						break;
-				}
+				case 0: // ignore, empty tile
+					break;
+				case 1: // HQ
+					resource_delta.x += 3;
+					resource_delta.y += 3;
+					resource_delta.z += 3;
+					break;
+				case 2: // mine
+					resource_delta.y += 3;
+					resource_delta.x -= 2;
+					break;
+				case 3: // power plant
+					resource_delta.x += 3;
+					break;
+				case 4: // farm
+					resource_delta.z += 3;
+					resource_delta.x -= 1;
+					break;
+				case 5: // shipyard(fighter), produces 1 ship every 5 seconds
+					if (tick && ((int)game.gameAge) % 2 == 0 && game.planets[i].shipPresence[0] < game.planets[i].radius)
+					{
+						float a = (rand() % 360) / (180.f/3.41f);
+						Ship* s = spawnShip(vecadd(vecf(cos(a)*game.planets[i].radius, sin(a)* game.planets[i].radius), game.planets[i].position), 0, 0);
+						s->velocity = normalize(vecsub(s->position, game.planets[i].position));
+					}
+					break;
+				case 6: // shipyard(bomber), produces 1 ship every 15 seconds
+					if (tick && ((int)game.gameAge) % 10 == 0 && game.planets[i].shipPresence[1] < game.planets[i].radius)
+					{
+						float a = (rand() % 360) / (180.f/3.41f);
+						Ship* s = spawnShip(vecadd(vecf(cos(a)*game.planets[i].radius, sin(a)* game.planets[i].radius), game.planets[i].position), 1, 0);
+						s->velocity = normalize(vecsub(s->position, game.planets[i].position));
+					}
+					break;
+				case 7: // shipyard(cruiser), produces 1 ship every 60 seconds
+					if (tick && ((int)game.gameAge) % 30 == 0 && game.planets[i].shipPresence[2] < game.planets[i].radius)
+					{
+						float a = (rand() % 360) / (180.f/3.41f);
+						Ship* s = spawnShip(vecadd(vecf(cos(a)*game.planets[i].radius, sin(a)* game.planets[i].radius), game.planets[i].position), 2, 0);
+						s->velocity = normalize(vecsub(s->position, game.planets[i].position));
+					}
+					break;
 			}
 		}
 	}
@@ -561,6 +572,11 @@ void tickGame(float step)
 	for (int i = 0; i < game.numPlanets; i++)
 	{
 		Planet* p = &(game.planets[i]);
+		if (p->team == 0)
+		{
+			printf("%f\n", sqrt(p->radius));
+			resource_delta.z -= sqrt(p->radius); // subtract some food
+		}
 		p->shipPresence[0] = 0;
 		p->shipPresence[1] = 0;
 		p->shipPresence[2] = 0;
@@ -580,6 +596,8 @@ void tickGame(float step)
 	for (int i = 0; i < game.numShips; i++)
 	{
 		Ship* s = &game.ships[i];
+		if (s->team == 0)
+			resource_delta.x -= s->values->energyUsage; // subtract some energy
 		Vectorf force;
 		force = vecf(0.f, 0.f);
 		if (s->target == NULL) // cruise mode
@@ -680,6 +698,14 @@ void tickGame(float step)
 		// normalize velocity and adjust it as per type
 		s->position = vecadd(s->position, vecscale(s->velocity, step * s->values->speed));
 	}
+
+	Vectorf resource_delta_scaled = vecscale(resource_delta, step); // make sure to advance the counters only by a fraction based on the time passeds
+
+	game.resources[rsc_energy] += resource_delta_scaled.x;
+	game.resources[rsc_sbm] += resource_delta_scaled.y;
+	game.resources[rsc_food] += resource_delta_scaled.z;
+
+	printf("Resources: %f %f %f, delta %f %f %f\n", game.resources[0], game.resources[1], game.resources[2], resource_delta.x, resource_delta.y, resource_delta.z);
 }
 
 void loadAssets()
